fbdd9501f554a658e2c471991b821b6f6dc51866
nikki-8 <raman.swati2011@gmail.com>
2025-01-22 02:23:05 +0530

    first commit

---
+++ src/Singleton.java
@@ -0,0 +1,21 @@
+//public class Singleton {
+//    public static void main(String[] args){
+//
+//    }
+//}
+//class Demo{
+//    private static  Demo d;
+//    private Demo() {
+//
+//    }
+//    public static Demo getInstance(){
+//        if (d==null){
+//            synchronized (Demo){
+//                if(d==null)
+//                    d=new Demo();
+//            }
+//            return d;
+//        }
+//    }
+//}
+//
